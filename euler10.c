5edbfebcf521f5586123e1615c8fe527fa6f02ee
Anh <kanzungjak@gmail.com>
2013-03-24 23:19:39 +0100

    Add 10

--- euler10.c
+++ euler10.c
@@ -1,28 +1,36 @@
+//The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+//Find the sum of all the primes below two million.
+
 #include <stdio.h>
 #include <math.h>
 
+long isPrime(long n) {
+		if(n == 2) 
+			return 1;
+		if(n % 2 == 0 || n == 1) {
+			return 0;
+		}
+		long sq = sqrt(n) + 1;
+		long i;
+		for(i = 3; i < sq; i++) {
+			if(n % i == 0) {
+				return 0;
+			}
+		}
+		return 1;
+}	
 
 int main(int argc, char **argv) {	
 	long N = 2000000;
-	char *bitmap = (char*)malloc(N * sizeof(char));
-	long i,j;
-	for(i = 0; i < N; i++) {
-		bitmap[i] = '0';
-	}
+	long i;
 	
-	for(i = 2; i < N; i++) {
-		if( bitmap[i] == '0') {
-				for(j = i*i; j < N; j+=i) {
-						bitmap[j] = '1';
-				}
+	double sum = 2; //bei int gabs nen Überlauf ...
+	for(i = 3; i < N; i += 2) {
+		if(isPrime(i)) {
+			sum += i;
 		}
 	}
-	
-	/*
-	for(i = 0; i < 1000; i++) {
-			printf("%c ", bitmap[i]);
-	}
-*/
+	printf("sum %f\n", sum);
 
 	return 0;
 }
